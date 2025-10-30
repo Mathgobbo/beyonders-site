d248c50c6d520b6c070624c51645abab0641876a
Mathgobbo <matgobbo@hotmail.com>
2025-10-30 09:56:30 -0300

    chore: Changed vbout to _document.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,25 +45,6 @@ export default function App({ Component, pageProps }: AppProps) {
             <Footer />
           </main>
         </LazyMotion>
-
-        {/* Vbout script tag */}
-        <Script
-          id="vbout-tag"
-          dangerouslySetInnerHTML={{
-            __html: ` 
-            /* embed code starts */
-            var _vbset = _vbset || [];
-            _vbset.push(['_account', 'VBT-05131-15326']);
-            _vbset.push(['_domain', 'https://beyonders.agency']);
-
-            (function() {
-              var vbt = document.createElement('script'); vbt.type = 'text/javascript'; vbt.async = true; 
-              vbt.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.vbt.io/tracker?_account='+_vbset[0][1]+'&_domain='+_vbset[1][1];var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vbt, s);
-            })();
-            /* embed code ends */ 
-            `,
-          }}
-        />
       </main>
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
+        {/* Vbout script tag */}
+        <Script
+          id="vbout-tag"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` 
+            /* embed code starts */
+            var _vbset = _vbset || [];
+            _vbset.push(['_account', 'VBT-05131-15326']);
+            _vbset.push(['_domain', 'https://beyonders.agency']);
+
+            (function() {
+              var vbt = document.createElement('script'); vbt.type = 'text/javascript'; vbt.async = true; 
+              vbt.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.vbt.io/tracker?_account='+_vbset[0][1]+'&_domain='+_vbset[1][1];var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vbt, s);
+            })();
+            /* embed code ends */ 
+            `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
